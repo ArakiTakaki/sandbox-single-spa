e5b54af3ba7754ba57bbf585c2da1e005184b63b
ArakiTakaki <araki_takaki@yahoo.co.jp>
2020-06-14 04:31:11 +0900

    ok

--- packages/page1/build.ts
+++
@@ -1,9 +0,0 @@
-import { webpackAsync } from "../../scripts/webpack/util/polyfill";
-import config from './webpack.config'
-webpackAsync(config)
-  .then(() => {
-    console.log('done');
-  })
-  .catch(() => {
-    console.error('dev server fail');
-  });
